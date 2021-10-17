977c117981b8d615860349a22eebadefa479e7fe
dadu0699 <dadu0699@hotmail.com>
2021-10-17 11:35:40 -0600

    [UPDATE] Connection to Rekognition

--- api-flask/app.py
+++ api-flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from rekognition import labels
+from rekognition import detect_labels
 
 app = Flask(__name__)
 
@@ -9,9 +9,9 @@ def health_check():
     return jsonify({"status": "ok"})
 
 
-@app.route('/tarea3-201801266', methods=['POST'])
+@app.route('/detect-labels', methods=['POST'])
 def tarea3():
-    return jsonify(labels(request.json['image']))
+    return jsonify(detect_labels(request.json['image']))
 
 
 if __name__ == '__main__':
--- api-flask/rekognition.py
+++ api-flask/rekognition.py
@@ -9,13 +9,14 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 REGION_NAME = os.getenv('REGION_NAME')
 
+client = boto3.client(
+    'rekognition',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=REGION_NAME
+)
 
-def labels(img64):
+
+def detect_labels(img64):
     decoded_image = base64.b64decode(img64)
-    client = boto3.client(
-        'rekognition',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=REGION_NAME
-    )
     return client.detect_labels(Image={'Bytes': decoded_image})
